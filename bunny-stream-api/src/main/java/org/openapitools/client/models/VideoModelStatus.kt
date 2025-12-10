815cd0817954fa77d94f20500e64e7f5bd753018
abdalla atheer <abdalla1997733@gmail.com>
2025-12-10 11:41:33 +0300

    wip

---
+++ bunny-stream-api/src/main/java/org/openapitools/client/models/VideoModelStatus.kt
@@ -0,0 +1,9 @@
+/**
+ * This is a placehodler class. The actual implementation is provided by typeMappings in GenerateTask config.
+ */
+package org.openapitools.client.models
+
+// This class replaces the (wrong) auto-generated implementation
+class VideoModelStatus {
+    // Intentionally left empty
+}
